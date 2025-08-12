d5c1869081e34e3522a8cfef2886c117cef272bf
Mohsin-khan540 <arhamk4555@gmail.com>
2025-08-12 16:31:11 +0500

    Initial Commit

---
+++ IExpense App/IExpense_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IExpense_AppApp.swift
+//  IExpense App
+//
+//  Created by Mohsin khan on 12/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct IExpense_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IExpense AppTests/IExpense_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  IExpense_AppTests.swift
+//  IExpense AppTests
+//
+//  Created by Mohsin khan on 12/08/2025.
+//
+
+import Testing
+@testable import IExpense_App
+
+struct IExpense_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
